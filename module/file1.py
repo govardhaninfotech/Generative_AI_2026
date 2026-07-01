0c40d35c766b1c29d7759ad52b2b3618fcf34107
govardhaninfotech <infotechg87@gmail.com>
2026-07-01 18:57:41 +0530

    .

---
+++ module/file1.py
@@ -0,0 +1,10 @@
+def display1():
+    print("welcome to display 1 from file1")
+    
+def check_display():
+    print("welcome to check_display  from file1")
+    
+display1()
+
+if __name__ == "__main__":
+    check_display()
---
+++ module/main.py
@@ -0,0 +1,4 @@
+import file1
+
+# print(file1.__name__)
+# print(__name__)
